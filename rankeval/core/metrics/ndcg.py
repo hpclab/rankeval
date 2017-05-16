32518ec1c828bb1e3ad5f7170c82a29532928871
Franco Maria Nardini <francomaria.nardini@isti.cnr.it>
2017-05-16 13:10:13 +0200

    adding metrics

---
+++ rankeval/core/metrics/ndcg.py
@@ -0,0 +1,19 @@
+import ...
+
+def ndcg(scores, labels, q_lens, cutoff = 10, no_relevant_results = 'Yahoo'):
+    no_relevant_results_score = 0.0
+
+    if (no_relevant_results = 'Yahoo'):
+        no_relevant_results_score = 0.5
+
+    # aggiungere NDCG di HPCKit
+
+    return ndcg
+
+
+
+def ndcg_perquery(scores, labels, q_lens, cutoff = 10, no_relevant_results = 'Yahoo'):
+    pass
+
+def ndcg_tied(scores, labels, q_lens, cutoff = 10, no_relevant_results = 'Yahoo'):
+    pass
