f8bf41998facc1310b0b8bbd3105c603c37ee49d
Cristina Ioana Muntean <muntean@cris-macmini.isti.cnr.it>
2017-06-01 09:44:25 +0200

    test ndcg

--- rankeval/test/core/metrics/test_metrics.py
+++ rankeval/test/core/metrics/test_metrics.py
@@ -3,25 +3,27 @@ import unittest
 
 
 from numpy.testing import assert_equal, assert_almost_equal
-import os
-from rankeval.core.dataset import Dataset
+# import os
+# from rankeval.core.dataset import Dataset
+from rankeval.core.metrics.ndcg import NDCG
+
 from rankeval.core.metrics.dcg import DCG
 from rankeval.core.metrics.precision import Precision
-from rankeval.core.model import ProxyQuickRank
-from rankeval.core.scoring import Scorer
-from rankeval.test.base import data_dir
-
-model_file = os.path.join(data_dir, "quickrank.model.xml")
-data_file = os.path.join(data_dir, "msn1.fold1.test.5k.txt")
+# from rankeval.core.model import ProxyQuickRank
+# from rankeval.core.scoring import Scorer
+# from rankeval.test.base import data_dir
+#
+# model_file = os.path.join(data_dir, "quickrank.model.xml")
+# data_file = os.path.join(data_dir, "msn1.fold1.test.5k.txt")
 from rankeval.core.metrics.recall import Recall
 
 
 class MetricsTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.model = ProxyQuickRank.load(model_file)
-        self.dataset = Dataset(data_file, format="svmlight")
-        self.scorer = Scorer(self.model, self.dataset)
+        # self.model = ProxyQuickRank.load(model_file)
+        # self.dataset = Dataset(data_file, format="svmlight")
+        # self.scorer = Scorer(self.model, self.dataset)
 
         self.query_scores = np.array([2.3, 0.0, 0.1, 5.5, 1.0])
         self.query_labels = np.array([2, 3, 0, 1, 0])
@@ -100,4 +102,16 @@ class MetricsTestCase(unittest.TestCase):
         p = DCG()
         result = p.eval_per_query(self.query_labels, self.query_scores)
         assert_almost_equal(result, 3.422, decimal=3)
-        pass
+
+    def test_DCG_eval_per_query_IDCG(self):
+        p = DCG()
+        result = p.eval_per_query(self.query_labels, self.query_labels)
+        assert_almost_equal(result, 4.761, decimal=3)
+
+
+    # NDCG
+
+    def test_NDCG_eval_per_query(self):
+        p = NDCG()
+        result = p.eval_per_query(self.query_labels, self.query_scores)
+        assert_almost_equal(result, 0.718, decimal=3)
