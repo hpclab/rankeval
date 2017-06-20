278457ae5ff113c3642b98340c28901a5d1bde5b
Claudio Lucchese <claudio.lucchese@isti.cnr.it>
2017-06-20 14:49:34 +0200

    statistical significance given model and metrics

--- rankeval/analysis/statistical.py
+++ rankeval/analysis/statistical.py
@@ -15,8 +15,56 @@ This package implements several statistical significance tests.
 import numpy as np
 import xarray as xr
 
+import itertools
 
-def randomization(metric_scores_A, metric_scores_B, n_perm=100000):
+def statistical_significance(datasets, model_a, model_b, metrics, n_perm=100000):
+	"""
+	This method computes the statistical significance of the performance difference between model_a and.
+
+	Parameters
+	----------
+	datasets : list of Dataset
+		The datasets to use for analyzing the behaviour of the model using the given metrics and models
+	model_a : RTEnsemble
+		The first model considered.
+	model_b : RTEnsemble
+		The second model considered.
+	metrics : list of Metric
+		The metrics to use for the analysis
+	n_perm : int
+		Number of permutations for the randomization test.
+
+	Returns
+	-------
+	stat_sig : xarray.DataArray
+		A DataArray containing the statistical significance of the performance difference 
+		between any pair of models on the given dataset,
+	"""
+
+	data = np.zeros(shape=(len(datasets), len(metrics), 2), dtype=np.float32)
+	for idx_dataset, dataset in enumerate(datasets):
+		scorer_a = model_a.score(dataset, detailed=False)
+		scorer_b = model_b.score(dataset, detailed=False)
+		for idx_metric, metric in enumerate(metrics):
+			metrics_a = metric.eval(dataset, scorer_a.y_pred)[1]
+			metrics_b = metric.eval(dataset, scorer_b.y_pred)[1]
+
+			p1, p2 = _randomization(metrics_a, metrics_b, n_perm=n_perm)
+
+			data[idx_dataset][idx_metric][0] = p1
+			data[idx_dataset][idx_metric][1] = p2
+
+	performance = xr.DataArray(data,
+							   name='Statistical Significance',
+							   coords=[datasets, metrics, ['one-sided','two-sided'] ],
+							   dims=['dataset', 'metric', '1-2 sided'])
+
+	return performance
+
+
+
+
+def _randomization(metric_scores_A, metric_scores_B, n_perm=100000):
 	"""
 	This method computes the randomization test as described in [1].
 
@@ -62,10 +110,10 @@ def randomization(metric_scores_A, metric_scores_B, n_perm=100000):
 	for i in range(n_perm):
 		# select a random subset
 		sel = np.random.choice([False, True], len(metric_scores_A))
-
+		# compute avg performance of randomized models
 		A_mean = (np.sum(best_metrics [np.logical_not(sel)]) + np.sum(worst_metrics[sel]) )/N
 		B_mean = (np.sum(worst_metrics[np.logical_not(sel)]) + np.sum(best_metrics [sel]) )/N
-
+		# performance difference
 		delta = A_mean - B_mean
 
 		if delta>=difference:
@@ -78,3 +126,6 @@ def randomization(metric_scores_A, metric_scores_B, n_perm=100000):
 
 
 	return (p1,p2)
+
+
+
--- rankeval/test/analysis/test_statistical.py
+++ rankeval/test/analysis/test_statistical.py
@@ -5,24 +5,45 @@ import logging
 import numpy as np
 from numpy.testing import assert_array_almost_equal, assert_almost_equal
 
-from rankeval.analysis.statistical import randomization
+from rankeval.analysis.statistical import statistical_significance
+from rankeval.analysis.statistical import _randomization
+
+from rankeval.core.model import RTEnsemble
+from rankeval.core.scoring.scorer import Scorer
+from rankeval.core.dataset import Dataset
+from rankeval.core.metrics.ndcg import NDCG
+
+from rankeval.test.base import data_dir
+
+
 
 class StatisticalSignificanceTestCase(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.model_a = RTEnsemble(os.path.join(data_dir, "quickrank.model.xml"), format="quickrank")
+        self.model_b = RTEnsemble(os.path.join(data_dir, "quickrank.model.v2.xml"), format="quickrank")
+        self.dataset = Dataset(os.path.join(data_dir, "msn1.fold1.test.5k.txt"), format="svmlight")
+        self.metric = NDCG()
 
     def tearDown(self):
-        pass
+        del self.model_a
+        self.model_a = None
+        del self.model_b
+        self.model_b = None
+        del self.dataset
+        self.dataset = None
+        del self.metric
+        self.metric = None
  
+    def test_statistical_significance(self):
+    	data = statistical_significance([self.dataset], self.model_a, self.model_b, [self.metric], n_perm=100)
+
+
     def test_randomization(self):
         A = np.array([1,1,1,1,1,1,1,0,0,0])
         B = np.array([0,0,0,0,0,0,0,1,1,1])
         n_perm = 10000
-
-
-        p1,p2 = randomization( A, B, n_perm)
-
+        p1,p2 = _randomization( A, B, n_perm)
         assert_almost_equal(p2, .34195, decimal=2) # compute with https://github.com/searchivarius/PermTest
 
 
