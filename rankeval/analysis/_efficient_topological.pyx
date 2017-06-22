061f2046164a1ac79bf7de37d711288d68811160
Salvatore Trani <trani.salvatore@gmail.com>
2017-06-22 15:07:21 +0200

    converted the signed long into a unsigned long in order to support the analysis of ensemble with an height up to 64 levels.

--- rankeval/analysis/_efficient_topological.pyx
+++ rankeval/analysis/_efficient_topological.pyx
@@ -14,26 +14,23 @@ import scipy.sparse
 # _always_ do that, or you will have segfaults
 np.import_array()
 
-cdef extern from "<math.h>" nogil:
-    double frexp(double value, int *exp)
-
 from cython.parallel import prange, parallel
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def efficient_topological_analysis(model, include_leaves=True):
-    
+
     cdef np.intp_t n_trees = model.n_trees
     cdef np.intp_t n_nodes = model.n_nodes
 
     cdef int[:] trees_root = model.trees_root
     cdef int[:] trees_left_child = model.trees_left_child
     cdef int[:] trees_right_child  = model.trees_right_child
-    
-    node_indices = np.full(model.n_nodes, fill_value=-1, dtype=np.int64)
-    cdef long[:] node_indices_view = node_indices
+
+    node_indices = np.zeros(model.n_nodes, dtype=np.uint64)
+    cdef unsigned long long[:] node_indices_view = node_indices
     cdef unsigned int[:] height_trees = np.zeros(model.n_trees, dtype=np.uint32)
-    
+
     cdef bint c_include_leaves = include_leaves
 
     cdef np.intp_t idx_tree
@@ -49,27 +46,31 @@ def efficient_topological_analysis(model, include_leaves=True):
                                                            idx_last_node,
                                                            c_include_leaves)
 
-    # Skip -1 values (leaves node not to be counted)
+    print node_indices
+
     # Computes unique indices and counts the occurrences of each index (aggregate)
-    unique_counts = np.unique(node_indices[node_indices != -1], return_counts=True)
-    
-    cdef long[:] data_indices_view = unique_counts[0]
-    cdef int[:] counts_view = unique_counts[1].astype(np.int32)
-    
+    unique_counts = np.unique(node_indices, return_counts=True)
+
+    cdef unsigned long long[:] data_indices_view = unique_counts[0]
+    cdef long[:] counts_view = unique_counts[1]
+
+    # overwrite counts of 0-values since they should identify only the
+    # root nodes but include also the leaves when include_leaves=False)
+    counts_view[0] = n_trees
+
     cdef np.intp_t data_indices_size = data_indices_view.size
 
     # indices in a sparse matrix representation
-    cdef unsigned long[:] row_ind = np.zeros(data_indices_size, dtype=np.uint64)
-    cdef unsigned long[:] col_ind = np.zeros(data_indices_size, dtype=np.uint64)
-    
+    cdef unsigned long long[:] row_ind = np.zeros(data_indices_size, dtype=np.uint64)
+    cdef unsigned long long[:] col_ind = np.zeros(data_indices_size, dtype=np.uint64)
+
     cdef np.intp_t idx_data
     cdef int exp
     with nogil, parallel():
         for idx_data in prange(data_indices_size):
-            frexp(data_indices_view[idx_data] + 1, &exp)
-            row_ind[idx_data] = exp - 1
+            row_ind[idx_data] = most_significant_bit(data_indices_view[idx_data] + 1)
             col_ind[idx_data] = data_indices_view[idx_data] + 1 - 2**row_ind[idx_data]
-            
+
     return sc.sparse.csr_matrix((counts_view, (row_ind, col_ind))), np.asarray(height_trees)
 
 @cython.boundscheck(False)
@@ -78,27 +79,26 @@ cdef int _compute_node_indices(np.intp_t idx_tree,
                                int[:] trees_root,
                                int[:] trees_left_child,
                                int[:] trees_right_child,
-                               long[:] node_indices,
+                               unsigned long long[:] node_indices,
                                int idx_last_node,
                                bint include_leaves) nogil:
 
     cdef int cur_node = trees_root[idx_tree]
-    node_indices[cur_node] = 0
+    cdef unsigned long long left_value, right_value, max_index = 0
     while cur_node < idx_last_node:
         if _is_leaf_node(cur_node, trees_left_child, trees_right_child):
             if not include_leaves:
-                node_indices[cur_node] = -1
+                node_indices[cur_node] = 0
         else:
-            node_indices[trees_left_child[cur_node]] = 2 * node_indices[cur_node] + 1
-            node_indices[trees_right_child[cur_node]] = 2 * node_indices[cur_node] + 2
+            left_value = 2 * node_indices[cur_node] + 1
+            right_value = 2 * node_indices[cur_node] + 2
+            node_indices[trees_left_child[cur_node]] = left_value
+            node_indices[trees_right_child[cur_node]] = right_value
+            max_index = max(max_index, left_value)
+            max_index = max(max_index, right_value)
         cur_node += 1
-    
-    cur_node = 0
-    cdef long max_index = 0
-    for cur_node in xrange(trees_root[idx_tree], idx_last_node):
-        max_index = max(max_index, node_indices[cur_node])
-    cdef int height
-    frexp(max_index + 1, &height)
+
+    cdef int height = most_significant_bit(max_index + 1)
     return height
 
 @cython.boundscheck(False)
@@ -107,3 +107,22 @@ cdef inline bint _is_leaf_node(int idx_node,
                                int[:] trees_left_child,
                                int[:] trees_right_child) nogil:
     return trees_left_child[idx_node] == -1 and trees_right_child[idx_node] == -1
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef int most_significant_bit(long long v) nogil:
+
+    cdef long long *b = [0x2, 0xC, 0xF0, 0xFF00, 0xFFFF0000, 0xFFFFFFFF00000000]
+    cdef unsigned int *S = [1, 2, 4, 8, 16, 32]
+
+    # result of log2(v) will go here
+    cdef unsigned int r = 0
+    # unroll for speed...
+    cdef int i = 5
+    while i >= 0:
+        if (v & b[i]):
+            v >>= S[i];
+            r |= S[i];
+        i -= 1
+    
+    return r
