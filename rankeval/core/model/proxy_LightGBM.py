387736d16e6f24852a34b6f311465d98a801874a
Salvatore Trani <trani.salvatore@gmail.com>
2017-07-11 13:56:39 +0200

    Introduced the LightGBM proxy loader

---
+++ rankeval/core/model/proxy_LightGBM.py
@@ -0,0 +1,207 @@
+# Copyright (c) 2017, All Contributors (see CONTRIBUTORS file)
+# Authors: Salvatore Trani <salvatore.trani@isti.cnr.it>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""Class providing the implementation for loading/storing a LightGBM model
+from/to file.
+
+The LightGBM project is described here:
+    https://github.com/Microsoft/LightGBM
+
+The LightGBM format adopts a textual representation using arrays for storing
+split nodes (both features and thresholds), leaf values and tree structure.
+
+// TODO
+There is an header section, identified by the "info" tag, with the most important parameters adopted to
+learn such a model. It follows then the description of the ensemble, with a node
+for each tree, identified by the "tree" tag, followed by the description of the
+tree (with splitting and leaf nodes). The splitting nodes are described with two
+ information: the feature id used for splitting, and the threshold value. Leaf
+nodes on the other hand are described by an "output" tag with the value as
+content.
+"""
+
+from rankeval.core.model.rt_ensemble import RTEnsemble
+
+import re
+import numpy as np
+
+tree_reg = re.compile("^Tree=(\d+)")
+num_leaves_reg = re.compile("^num_leaves=(\d+)")
+split_feature_reg = re.compile("^split_feature=(.*)")
+threshold_reg = re.compile("^threshold=(.*)")
+decision_type_reg = re.compile("^decision_type=(.*)")
+left_child_reg = re.compile("^left_child=(.*)")
+right_child_reg = re.compile("^right_child=(.*)")
+leaf_parent_reg = re.compile("^leaf_parent=(.*)")
+leaf_value_reg = re.compile("^leaf_value=(.*)")
+shrinkage_reg = re.compile("^shrinkage=(.*)")
+has_categorical_reg = re.compile("^has_categorical=(.*)")
+
+
+class ProxyLightGBM(object):
+    """
+    Class providing the implementation for loading/storing a QuickRank model from/to file.
+    """
+
+    @staticmethod
+    def load(file_path, model):
+        """
+        Load the model from the file identified by file_path.
+
+        Parameters
+        ----------
+        file_path : str
+            The path to the filename where the model has been saved
+        model : RTEnsemble
+            The model instance to fill
+        """
+        n_trees, n_nodes = ProxyLightGBM._count_nodes(file_path)
+        # Initialize the model and allocate the needed space
+        # given the shape and size of the ensemble
+        model.initialize(n_trees, n_nodes)
+
+        curr_tree = root_node = 0
+        num_leaves = num_splits = 0
+        with open(file_path, 'r') as f:
+            for line in f:
+
+                match = tree_reg.match(line)
+                if match:
+                    curr_tree = int(match.group(1))
+                    root_node += num_leaves + num_splits
+                    model.trees_root[curr_tree] = root_node
+                    continue
+
+                match = split_feature_reg.match(line)
+                if match:
+                    split_features = map(int, match.group(1).strip().split())
+                    for pos, feature in enumerate(split_features):
+                        # TODO: Check if feature starts from 1
+                        model.trees_nodes_feature[root_node + pos] = feature
+                    num_splits = len(split_features)
+                    continue
+
+                match = threshold_reg.match(line)
+                if match:
+                    thresholds = map(float, match.group(1).strip().split())
+                    for pos, threshold in enumerate(thresholds):
+                        model.trees_nodes_value[root_node + pos] = threshold
+                    continue
+
+                match = left_child_reg.match(line)
+                if match:
+                    left_children = map(int, match.group(1).strip().split())
+                    for pos, child in enumerate(left_children):
+                        if child >= 0:
+                            model.trees_left_child[root_node + pos] = \
+                                root_node + child
+                        else:
+                            model.trees_left_child[root_node + pos] = \
+                                root_node + num_splits + abs(child) - 1
+                    continue
+
+                match = right_child_reg.match(line)
+                if match:
+                    right_children = map(int, match.group(1).strip().split())
+                    for pos, child in enumerate(right_children):
+                        if child >= 0:
+                            model.trees_right_child[root_node + pos] = \
+                                root_node + child
+                        else:
+                            model.trees_right_child[root_node + pos] = \
+                                root_node + num_splits + abs(child) - 1
+                    continue
+
+                match = shrinkage_reg.match(line)
+                if match:
+                    # weight should be the shrinkage but it is set to 1 because
+                    # leaves output already take into account the shrinkage
+                    # shrinkage = float(match.group(1))
+                    model.trees_weight[curr_tree] = 1.0
+                    continue
+
+                match = leaf_value_reg.match(line)
+                if match:
+                    leaf_values = map(float, match.group(1).strip().split())
+                    num_leaves = len(leaf_values)
+                    for pos, leaf_value in enumerate(leaf_values):
+                        model.trees_nodes_value[root_node + num_splits + pos] \
+                            = leaf_value
+                    num_splits = len(split_features)
+                    continue
+
+                match = decision_type_reg.match(line)
+                if match:
+                    types = np.array(match.group(1).strip().split(), dtype=int)
+                    if types.any():
+                        raise AssertionError("Decision Tree not supported")
+                    continue
+
+                match = has_categorical_reg.match(line)
+                if match:
+                    categorical = bool(int(match.group(1)))
+                    if categorical:
+                        raise AssertionError("Decision Tree not supported")
+                    continue
+
+
+    @staticmethod
+    def save(file_path, model):
+        """
+        Save the model onto the file identified by file_path.
+
+        Parameters
+        ----------
+        file_path : str
+            The path to the filename where the model has to be saved
+        model : RTEnsemble
+            The model RTEnsemble model to save on file
+
+        Returns
+        -------
+        status : bool
+            Returns true if the save is successful, false otherwise
+        """
+        raise NotImplementedError("Feature not implemented!")
+
+    @staticmethod
+    def _count_nodes(file_path):
+        """
+        Count the total number of nodes (both split and leaf nodes)
+        in the model identified by file_path.
+
+        Parameters
+        ----------
+        file_path : str
+            The path to the filename where the model has been saved
+
+        Returns
+        -------
+        tuple(n_trees, n_nodes) : tuple(int, int)
+            The total number of trees and nodes (both split and leaf nodes)
+            in the model identified by file_path.
+        """
+
+        n_nodes = 0
+        n_trees = 0
+
+        with open(file_path, 'r') as f:
+            for line in f:
+                match = num_leaves_reg.match(line)
+                if match:
+                    n_nodes += int(match.group(1))
+                    continue
+                match = split_feature_reg.match(line)
+                if match:
+                    n_nodes += len(match.group(1).strip().split())
+                    continue
+                match = tree_reg.match(line)
+                if match:
+                    n_trees += 1
+                    continue
+
+        return n_trees, n_nodes
---
+++ rankeval/test/core/model/test_proxy_LightGBM.py
@@ -0,0 +1,88 @@
+import os
+import unittest
+import logging
+
+from numpy.testing import assert_equal, assert_array_equal, assert_array_almost_equal
+from rankeval.core.dataset import Dataset
+
+from rankeval.core.model import RTEnsemble
+
+from rankeval.core.model.proxy_LightGBM import ProxyLightGBM
+from rankeval.test.base import data_dir
+
+model_file = os.path.join(data_dir, "LightGBM.model.txt")
+data_file = os.path.join(data_dir, "msn1.fold1.test.5k.txt")
+
+
+class ProxyQuickRankTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.model = RTEnsemble(model_file, format="LightGBM")
+        cls.dataset = Dataset.load(data_file, format="svmlight")
+
+    @classmethod
+    def tearDownClass(cls):
+        del cls.model
+        cls.model = None
+        del cls.dataset
+        cls.dataset = None
+
+    def test_count_nodes(self):
+        n_trees, n_nodes = ProxyLightGBM._count_nodes(model_file)
+        # print "Num Trees: %d\nNum Nodes: %d" % (n_trees, n_nodes),
+        assert_equal(n_trees, 2)
+        assert_equal(n_nodes, 10)
+        assert_equal(n_trees, self.model.trees_root.size)
+        assert_equal(n_nodes, self.model.trees_nodes_value.size)
+
+    def test_root_nodes(self):
+        assert_equal((self.model.trees_root > -1).all(), True,
+                     err_msg="Root nodes not set correctly")
+
+    def test_root_nodes_adv(self):
+        assert_array_equal(self.model.trees_root, [0, 5],
+                           err_msg="Root nodes are not correct")
+
+    def test_split_features(self):
+        assert_array_equal(self.model.trees_nodes_feature,
+                           [55, 134, -1, -1, -1, 133, 48, -1, -1, -1])
+
+    def test_tree_values(self):
+        assert_array_almost_equal(self.model.trees_nodes_value,
+            [3.63099994e-03, 2.06000000e+02, -3.71787995e-02, -1.57113143e-04,
+             3.06654684e-02, 9.99999968e-21, 2.33031496e-01, -4.58943285e-03,
+             3.43261547e-02, 1.79146975e-02],
+            err_msg="Split thresholds or leaf outputs value are not correct")
+
+    def test_left_children(self):
+        assert_array_equal(self.model.trees_left_child,
+                           [2, 3, -1, -1, -1, 6, 7, -1, -1, -1])
+
+    def test_right_children(self):
+        assert_array_equal(self.model.trees_right_child,
+                           [1, 4, -1, -1, -1, 8, 9, -1, -1, -1])
+
+    def test_leaf_correctness(self):
+        for idx, feature in enumerate(self.model.trees_nodes_feature):
+            if feature == -1:
+                assert_equal(self.model.trees_left_child[idx], -1,
+                             "Left child of a leaf node is not empty (-1)")
+                assert_equal(self.model.trees_right_child[idx], -1,
+                             "Right child of a leaf node is not empty (-1)")
+                assert_equal(self.model.is_leaf_node(idx), True,
+                             "Leaf node not detected as a leaf")
+
+    def test_prediction(self):
+        scorer = self.model.score(self.dataset)
+        assert_array_almost_equal(scorer.y_pred[:5],
+                                  [-0.00474655, -0.00474655, -0.00474655,
+                                   -0.00474655, -0.00474655])
+        assert_array_almost_equal(scorer.y_pred[-5:],
+                                  [0.01775758, -0.00474655, -0.00474655,
+                                   -0.00474655, -0.00474655])
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+                        level=logging.DEBUG)
+    unittest.main()
