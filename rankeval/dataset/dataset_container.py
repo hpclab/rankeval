aad9a1108f701a065dc4b824258bbda0351a7771
Franco Maria Nardini <francomaria.nardini@isti.cnr.it>
2017-07-20 19:39:43 +0200

    second round of fetcher with JSON support and remote fetching of dataset catalogue

--- rankeval/dataset/dataset_container.py
+++ rankeval/dataset/dataset_container.py
@@ -6,29 +6,7 @@ class DatasetContainer(object):
 
     license_agreement = ''
 
+    models = None
+
     def __init__(self):
         return
-
-    def set_train_dataset(self, train):
-        self.train = train
-
-    def get_train_dataset(self):
-        return self.train_dataset
-
-    def set_validation_dataset(self, validation):
-        self.validation = validation
-
-    def get_validation_dataset(self):
-        return self.validation_dataset
-
-    def set_test_dataset(self, test):
-        self.test = test
-
-    def get_test_dataset(self):
-        return self.test_dataset
-
-    def set_license(self, license):
-        self.license_agreement = license
-
-    def get_license(self):
-        return self.license_agreement
