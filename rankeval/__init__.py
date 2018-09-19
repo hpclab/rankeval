fb9f158e802bcc6588863cca384826ea315bca02
Zeno Gantner <zeno.gantner@here.com>
2018-09-19 15:12:24 +0200

    fix issue #6: import problem with Python 3.6

--- rankeval/__init__.py
+++ rankeval/__init__.py
@@ -7,5 +7,5 @@ import io
 
 cur_dir = os.path.dirname(__file__)
 
-__version__ = io.open(os.path.join(cur_dir, '..', 'VERSION'),
+__version__ = io.open(os.path.join(cur_dir, 'VERSION'),
                       encoding='utf-8').read().strip()
