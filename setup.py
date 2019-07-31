db5612705ad78fe8145dcd5135c2c432caaa68a7
Salvatore Trani <trani.salvatore@gmail.com>
2019-07-31 09:47:30 +0200

    improved setup such as to compile with OpenMP enabled only if the compiler supports it. The installation now works also with clang

--- setup.py
+++ setup.py
@@ -14,15 +14,44 @@ import sys
 import os
 import io
 import shutil
+import contextlib
 
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 from distutils.command.clean import clean as Clean
+from distutils import log as _log
+from distutils.dir_util import mkpath as _mkpath
+import tempfile as _tempfile
+from distutils.errors import CCompilerError as _CCompilerError
 
 
 if (sys.version_info[:1] == 2 and sys.version_info[:2] < (2, 7)) or \
     (sys.version_info[:1] == 3 and sys.version_info[:2] < (3, 5)):
     raise Exception('This version of rankeval needs Python 2.7, 3.5 or later.')
+
+
+@contextlib.contextmanager
+def stdchannel_redirected(stdchannel, dest_filename):
+    """
+    A context manager to temporarily redirect stdout or stderr
+
+    e.g.:
+
+    with stdchannel_redirected(sys.stderr, os.devnull):
+        if compiler.has_function('clock_gettime', libraries=['rt']):
+            libraries.append('rt')
+    """
+
+    try:
+        oldstdchannel = os.dup(stdchannel.fileno())
+        dest_file = open(dest_filename, 'w')
+        os.dup2(dest_file.fileno(), stdchannel.fileno())
+        yield
+    finally:
+        if oldstdchannel is not None:
+            os.dup2(oldstdchannel, stdchannel.fileno())
+        if dest_file is not None:
+            dest_file.close()
 
 
 class custom_build_ext(build_ext):
@@ -40,6 +69,55 @@ class custom_build_ext(build_ext):
 
         import numpy as np
         self.include_dirs.append(np.get_include())
+
+    def _check_openmp_support(self):
+        # Compile a test program to determine if compiler supports OpenMP.
+        _mkpath(self.build_temp)
+        _log.set_threshold(-1)
+        with stdchannel_redirected(sys.stderr, os.devnull):
+            with _tempfile.NamedTemporaryFile(mode='w',
+                                              dir=self.build_temp,
+                                              prefix='openmptest',
+                                              suffix='.c') as srcfile:
+                _log.info("checking if compiler supports OpenMP")
+                srcfile.write("""
+                #include <omp.h>
+                int testfunc() {
+                    int i;
+                    #pragma omp parallel for
+                    for (i = 0; i < 10; i ++)
+                        ;
+                    return omp_get_num_threads();
+                }
+                """)
+                srcfile.flush()
+                try:
+                    objects = self.compiler.compile([srcfile.name],
+                                                    extra_postargs=["-fopenmp"],
+                                                    output_dir="/")
+                except _CCompilerError:
+                    _log.info("compiler does not support OpenMP")
+                    use_openmp = False
+                else:
+                    _log.info("enabling OpenMP support")
+                    use_openmp = True
+                    for o in objects:
+                        os.remove(o)
+            return use_openmp
+
+    def build_extensions(self):
+        use_openmp = self._check_openmp_support()
+        if use_openmp:
+            for ext in self.extensions:
+                if not ext.extra_compile_args:
+                    ext.extra_compile_args = []
+                ext.extra_compile_args.append('-fopenmp')
+                if not ext.extra_link_args:
+                    ext.extra_link_args = []
+                ext.extra_link_args.append('-fopenmp')
+
+        # Chain to method in parent class.
+        build_ext.build_extensions(self)
 
 
 # Custom clean command to remove build artifacts
@@ -85,12 +163,12 @@ cmdclass = {
 }
 
 version = io.open(os.path.join(rankeval_dir, 'VERSION'),
-                  encoding='utf-8').read().strip(),
+                  encoding='utf-8').read().strip()
 readme = io.open(os.path.join(root_dir, 'README.md'), encoding='utf-8').read()
 
 setup(
     name='rankeval',
-    version=open(os.path.join(rankeval_dir, 'VERSION')).read().strip(),
+    version=version,
     description='Tool for the analysis and evaluation of Learning to Rank '
                 'models based on ensembles of regression trees.',
     long_description=readme,
@@ -105,20 +183,20 @@ setup(
         Extension('rankeval.scoring._efficient_scoring',
                   sources=[scoring_dir + '/_efficient_scoring.pyx'],
                   include_dirs=[scoring_dir],
-                  extra_compile_args=['-fopenmp', '-O3'],
-                  extra_link_args=['-fopenmp'],),
+                  extra_compile_args=['-O3'],
+                  ),
         Extension('rankeval.analysis._efficient_topological',
                   sources=[analysis_dir + '/_efficient_topological.pyx'],
                   include_dirs=[analysis_dir],
-                  extra_compile_args=['-fopenmp', '-O3'],
-                  extra_link_args=['-fopenmp'], ),
+                  extra_compile_args=['-O3'],
+                  ),
         Extension('rankeval.analysis._efficient_feature',
                   sources=[analysis_dir + '/_efficient_feature.pyx',
                            analysis_dir + '/_efficient_feature_impl.cpp'],
                   include_dirs=[analysis_dir],
                   language='c++',
-                  extra_compile_args=['-fopenmp', '-O3', "-w", "-std=c++11"],
-                  extra_link_args=['-fopenmp'], )
+                  extra_compile_args=['-O3', "-w", "-std=c++11"],
+                  )
     ],
 
     cmdclass=cmdclass,
@@ -131,7 +209,8 @@ setup(
     url='http://rankeval.isti.cnr.it',
     download_url='http://pypi.python.org/pypi/rankeval',
 
-    keywords='Learning to Rank, Model Analysis, Model Evaluation, Ensemble of Regression Trees',
+    keywords='Learning to Rank, Model Analysis, Model Evaluation, '
+             'Ensemble of Regression Trees',
 
     platforms='any',
 
