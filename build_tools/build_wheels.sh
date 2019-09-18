9a38d8d572495025b49638d9b372c4a85fde51e0
Salvatore Trani <trani.salvatore@gmail.com>
2019-09-18 15:02:25 +0200

    Final version with source distribution fixed

--- build_tools/build_wheels.sh
+++ build_tools/build_wheels.sh
@@ -2,42 +2,41 @@
 
 set -e -x
 
-# Install compilation requirements
-yum -y install freetype-devel pkg-config libpng-devel
-
 PACKAGE=rankeval
 BASE_PYTHON=/opt/python/cp37-cp37m/bin/
 
 ${BASE_PYTHON}/pip install -U twine
 
+cd /io/
+
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
 
-    "${PYBIN}/pip" install -U pip
+    ${PYBIN}/pip install -U pip
 
     # install requirement sequentially
-    cat /io/build_tools/requirements.txt | xargs -L 1 "${PYBIN}/pip" install
+    cat build_tools/requirements.txt | xargs -L 1 ${PYBIN}/pip install
 
-    (cd /io/; "${PYBIN}/python" setup.py clean)
-    "${PYBIN}/python" /io/setup.py sdist -d /io/wheelhouse/ -v
-    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+    ${PYBIN}/python setup.py clean
+    ${PYBIN}/python setup.py sdist -d dist/
+    ${PYBIN}/pip wheel ./ -w wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels and fix naming
 for whl in wheelhouse/*.whl; do
-    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+    auditwheel repair "$whl" --plat $PLAT -w dist/
 done
 
 # Test
 for PYBIN in /opt/python/*/bin; do
 
-    "${PYBIN}/pip" install --no-index -f /io/wheelhouse ${PACKAGE}
-    (cd "$HOME"; "${PYBIN}/nosetests" ${PACKAGE})
+    ${PYBIN}/pip install --no-index -f dist/ ${PACKAGE}
+    ${PYBIN}/nosetests ${PACKAGE}
 done
 
 #  Upload
-for WHEEL in /io/wheelhouse/${PACKAGE}*; do
-    "${BASE_PYTHON}/twine" upload \
+for WHEEL in dist/${PACKAGE}*; do
+    ${BASE_PYTHON}/twine upload \
         --skip-existing \
         -u "${PYPI_USER}" -p "${PYPI_PASS}" \
         "${WHEEL}"
